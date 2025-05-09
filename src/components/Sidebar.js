be44653aca4d493ac14350303149e9614b99981c
poorna <poornasurapuraju28@gmail.com>
2025-05-09 14:33:57 +0530

    sidebar ui changes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,25 +9,17 @@ const Sidebar = () => {
   const [isExpanded, setIsExpanded] = useState(true);
   const [isProfileExpanded, setIsProfileExpanded] = useState(false);
   const [isSettingsExpanded, setIsSettingsExpanded] = useState(false);
+  const [hoveredItem, setHoveredItem] = useState(null);
 
-  // Toggle sidebar size
   const toggleSidebar = () => setIsExpanded(!isExpanded);
-
-  // Toggle profile submenu
-  const handleProfileToggle = () => setIsProfileExpanded(!isProfileExpanded);
-
-  // Toggle settings submenu
-  const handleSettingsToggle = () => setIsSettingsExpanded(!isSettingsExpanded);
 
   return (
     <div className={`sidebar ${isExpanded ? "expanded" : "collapsed"}`}>
-      {/* Sidebar Header */}
       <div className="sidebar-header" onClick={toggleSidebar}>
         <BiListUl size={24} className="toggle-icon" />
         {isExpanded && <span className="sidebar-title">Dashboard</span>}
       </div>
 
-      {/* Sidebar Menu */}
       <div className="sidebar-menu">
         {/* Home */}
         <Link className="menu-item" to="/">
@@ -36,43 +28,90 @@ const Sidebar = () => {
         </Link>
 
         {/* Profile Section */}
-        <div className="menu-item" onClick={handleProfileToggle}>
-          <FaUserAlt size={20} />
-          {isExpanded && <span className="menu-text">Profile</span>}
-          {isExpanded && (isProfileExpanded ? <AiOutlineUp /> : <AiOutlineDown />)}
-          {!isExpanded && <AiOutlineRight className="collapsed-arrow" />}
-        </div>
-        {isProfileExpanded && isExpanded && (
-          <div className="submenu">
-            <Link className="submenu-item" to="/profile/student">
-              Student
-            </Link>
-            <Link className="submenu-item" to="/profile/parent">
-              Parent
-            </Link>
-            <Link className="submenu-item" to="/profile/employee">
-              Employee
-            </Link>
+        <div 
+          className="menu-item-container"
+          onMouseEnter={() => {
+            console.log("Mouse entered profile");
+            setHoveredItem("profile");
+          }}
+          onMouseLeave={() => {
+            console.log("Mouse left profile");
+            setHoveredItem(null);
+          }}
+        >
+          <div 
+            className="menu-item" 
+            onClick={() => {
+              console.log("Clicked profile");
+              setIsProfileExpanded(!isProfileExpanded);
+            }}
+          >
+            <FaUserAlt size={20} />
+            {isExpanded && <span className="menu-text">Profile</span>}
+            {isExpanded && (isProfileExpanded ? <AiOutlineUp /> : <AiOutlineDown />)}
+            {!isExpanded && <AiOutlineRight className="collapsed-arrow" />}
           </div>
-        )}
+          
+          {/* Expanded state submenu */}
+          {isProfileExpanded && isExpanded && (
+            <div className="submenu">
+              <Link className="submenu-item" to="/profile/student">Student</Link>
+              <Link className="submenu-item" to="/profile/parent">Parent</Link>
+              <Link className="submenu-item" to="/profile/employee">Employee</Link>
+            </div>
+          )}
+          
+          {/* Collapsed state hover submenu */}
+          {!isExpanded && hoveredItem === "profile" && (
+            <div className="hover-submenu">
+              <Link className="submenu-item" to="/profile/student">Student</Link>
+              <Link className="submenu-item" to="/profile/parent">Parent</Link>
+              <Link className="submenu-item" to="/profile/employee">Employee</Link>
+            </div>
+          )}
+        </div>
 
         {/* Settings Section */}
-        <div className="menu-item" onClick={handleSettingsToggle}>
-          <FaCog size={20} />
-          {isExpanded && <span className="menu-text">Settings</span>}
-          {isExpanded && (isSettingsExpanded ? <AiOutlineUp /> : <AiOutlineDown />)}
-          {!isExpanded && <AiOutlineRight className="collapsed-arrow" />}
-        </div>
-        {isSettingsExpanded && isExpanded && (
-          <div className="submenu">
-            <Link className="submenu-item" to="/settings/view-profile">
-              View Profile
-            </Link>
-            <Link className="submenu-item" to="/settings/edit-profile">
-              Edit Profile
-            </Link>
+        <div 
+          className="menu-item-container"
+          onMouseEnter={() => {
+            console.log("Mouse entered settings");
+            setHoveredItem("settings");
+          }}
+          onMouseLeave={() => {
+            console.log("Mouse left settings");
+            setHoveredItem(null);
+          }}
+        >
+          <div 
+            className="menu-item" 
+            onClick={() => {
+              console.log("Clicked settings");
+              setIsSettingsExpanded(!isSettingsExpanded);
+            }}
+          >
+            <FaCog size={20} />
+            {isExpanded && <span className="menu-text">Settings</span>}
+            {isExpanded && (isSettingsExpanded ? <AiOutlineUp /> : <AiOutlineDown />)}
+            {!isExpanded && <AiOutlineRight className="collapsed-arrow" />}
           </div>
-        )}
+          
+          {/* Expanded state submenu */}
+          {isSettingsExpanded && isExpanded && (
+            <div className="submenu">
+              <Link className="submenu-item" to="/settings/view-profile">View Profile</Link>
+              <Link className="submenu-item" to="/settings/edit-profile">Edit Profile</Link>
+            </div>
+          )}
+          
+          {/* Collapsed state hover submenu */}
+          {!isExpanded && hoveredItem === "settings" && (
+            <div className="hover-submenu">
+              <Link className="submenu-item" to="/settings/view-profile">View Profile</Link>
+              <Link className="submenu-item" to="/settings/edit-profile">Edit Profile</Link>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
